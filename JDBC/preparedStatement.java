c0fdb86f92e1178d37d9fae2347715ef8f8fc51a
Mohite Anith Kumar <mohiteanith@gmail.com>
2021-09-09 18:56:45 +0530

    FS

---
+++ JDBC/preparedStatement.java
@@ -0,0 +1,39 @@
+// java -cp "mysql-connector-java-8.0.16.jar;." preparedStatement
+
+import java.sql.*;
+
+class preparedStatement {
+  static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+  static final String DB_URL = "jdbc:mysql://localhost:3307/test";
+    
+  static final String USER = "root";
+  static final String PASS = "Anith26#200one";
+     
+  public static void main(String[] args) throws SQLException {  
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    
+    try{
+      Class.forName("com.mysql.jdbc.Driver");
+      System.out.println("Connecting to database...");
+
+      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+      pstmt = conn.prepareStatement("insert into dept values(?,?, ?)"); 
+      pstmt.setInt(1, 60);  //1 specifies the first parameter in the query  
+      pstmt.setString(2, "Marketing"); 
+      pstmt.setString(3,"hyderabad");
+
+      int i=pstmt.executeUpdate();  
+      System.out.println(i + " record inserted");        
+    }   
+    catch(Exception e){
+      e.printStackTrace();
+    }
+    finally
+    {
+      pstmt.close();
+      conn.close();
+    }
+  }
+}     
---
+++ test.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class test {
+    public String smallestSubsequence(String S) {
+        Stack<Integer> stack = new Stack<>();
+        int[] last = new int[26], seen = new int[26];
+        for (int i = 0; i < S.length(); ++i)
+            last[S.charAt(i) - 'a'] = i;
+
+
+        for (int i = 0; i < S.length(); ++i) {
+            int c = S.charAt(i) - 'a';
+            if (seen[c]++ > 0) continue;
+            while (!stack.isEmpty() && stack.peek() > c 
+				&& i < last[stack.peek()])
+                seen[stack.pop()] = 0;
+            stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i : stack) sb.append((char)('a' + i));
+        return sb.toString();
+    }
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		System.out.println(new test().smallestSubsequence(s));
+	}
+}
